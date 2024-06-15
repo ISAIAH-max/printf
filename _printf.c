0fc7f7913aa42c15b4b8f87f79c6c36b9ec5e7a3
ISAIAH-max <70812052+ISAIAH-max@users.noreply.github.com>
2024-06-15 03:07:43 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -8,69 +8,67 @@
  * (excluding the null byte used to end output to strings)
  */
 
-int _printf(const char *format, ...)
+int _printf(const char *format, ...) 
 {
-va_list args;
-int count = 0;
-
-va_start(args, format);
-
-while (*format)
-{
-if (*format == '%' && (*(format + 1) != '\0'))
-{
-  format++;
-  switch (*format)
-    {
-      case 'c':
-	count += handle_char(va_arg(args, int));
-	break;
-
-      case 's':
-	count += handle_string(va_arg(args, char *));
-	break;
-
-      case '%':
-	_putchar('%');
-	count++;
-	break;
-
-      default:
-	_putchar('%');
-	_putchar(*format);
-	count += 2;
-	break;
-    }
-}
-else
-{
-  _putchar(*format);
-  count++;
-}
-format++;
-}
-va_end(args);
-return (count);
+		
+	va_list args;
+		
+	int count = 0;
+	
+	va_start(args, format);
+	
+	while (*format)
+	{
+		if (*format == '%' && (*(format + 1) != '\0'))
+	{
+		format++;
+		switch (*format)
+		{
+			case 'c':
+				count += handle_char(va_arg(args, int));
+				break;
+			case 's':
+				count += handle_string(va_arg(args, char *));
+				break;
+			case '%':
+				_putchar('%');
+				count++;
+				break;
+			default:
+				_putchar('%');
+				_putchar(*format);
+				count += 2;
+				break;
+		}
+	}
+			else
+			{
+				_putchar(*format);
+				count++;
+			}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
 
 int handle_char(int c)
 {
-_putchar(c);
-return (1);
+	_putchar(c);
+	return (1);
 }
 
 int handle_string(char *s)
 {
-int count = 0;
+	int count = 0;
 
-if (!s)
-s = "(null)";
-
-while (*s)
-{
-_putchar(*s);
-s++;
-count++;
-}
-return (count);
+	if (!s)
+		s = "(null)";
+	while (*s)
+		{
+			_putchar(*s);
+			s++;
+			count++;
+		}
+	return (count);
 }
