f03e507d3a0bceb5b7bb3db74c2e9c3c2d81f281
isaiah-max <mlewaisaiah@gmail.com>
2024-06-18 01:11:55 +0000

    initial commit

---
+++ _printf_char.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _printf_char - print a character.
+ *@args: The arguments
+ *
+ * Return: 1
+ */
+
+int _printf_char(va_list args)
+{
+  char s;
+  s = va_arg(args, int);
+  _putchar(s);
+  return (1);
+}
