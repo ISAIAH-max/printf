bfe202dbc91dd8cd2b4445ad24215ba40817298c
ISAIAH-max <mlewaisaiah@gmail.com>
2024-07-08 20:40:21 +0300

    Update

--- _printf.c
+++ _printf.c
@@ -10,51 +10,52 @@
 
 int _printf(const char *format, ...)
 {
-  va_list args;
-  int i = 0, j = 0, count = 0;
-  printer_t funcs[] = {
-    {"c", _printf_char},
-    {"s", _printf_string},
-  };
+	va_list args;
+    int i = 0, j = 0, count = 0;
+    printer_t funcs[] = {
+		{"c", _printf_char},
+        {"s", _printf_string},
+	};
 
-  va_start(args, format);
-
-  while (format && (*(format + i)))
-  {
-    if (format[i] == '%')
-    {
-      i++;
-      if (format[i] == '%')
-      {
-	_putchar('%');
-	count++;
-      }
-      else
-      {
-	j = 0;
-	while (j < 2 && format[i] != *(funcs[j].symbol))
-	  j++;
-
-	if (j < 2)
+	va_start(args, format);
+	
+	while (format && (*(format + i)))
 	{
-	  funcs[j].print(args);
-	  count++;
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				j = 0;
+				while (j < 2 && format[i] != *(funcs[j].symbol))
+				{
+					j++;
+				}
+				if (j < 2)
+				{
+					funcs[j].print(args);
+					count++;
+				}
+				else
+				{
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+				}
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
 	}
-	else
-	{
-	  _putchar('%');
-	  _putchar(format[i]);
-	  count += 2;
-	}
-      }
-    }
-    else
-    {
-      _putchar(format[i]);
-      count++;
-    }
-    i++;
-  }
-  va_end(args);
-  return (count);
+	va_end(args);
+	return (count);
 }
