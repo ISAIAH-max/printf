badad374b0ef20223f81a6e3090fe4b8cdd91e3a
isaiah-max <mlewaisaiah@gmail.com>
2024-07-08 16:45:39 +0000

    Header file for my printf function

--- main.h
+++ main.h
@@ -5,12 +5,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+typedef struct printer
+{
+  char *symbol;
+  void (*print)(va_list args);
+} printer_t;
+
 /* Function prototype printf */
 int _printf(const char *format, ...);
 
 /* Function prototypes for handling different formats */
-int handle_char(char c);
-int handle_string(const char *s);
+void _printf_char(va_list args);
+void _printf_string(va_list args);
 
 /* Function prototype for for my own putchar */
 int _putchar(char c);
