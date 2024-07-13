52ca3e1f6543674eae7bb35b6e38fd33a2ea05fc
isaiah-max <mlewaisaiah@gmail.com>
2024-07-13 08:49:07 +0000

    Update

--- main.h
+++ main.h
@@ -8,15 +8,17 @@
 typedef struct printer
 {
   char *symbol;
-  void (*print)(va_list args);
+  int (*print)(va_list args);
 } printer_t;
 
 /* Function prototype printf */
 int _printf(const char *format, ...);
 
 /* Function prototypes for handling different formats */
-void _printf_char(va_list args);
-void _printf_string(va_list args);
+int _printf_char(va_list args);
+int _printf_string(va_list args);
+int _printf_int(va_list args);
+
 
 /* Function prototype for for my own putchar */
 int _putchar(char c);
