5071d45b5a2fc50fc542ca572aeb1914879d4bbd
ISAIAH-max <mlewaisaiah@gmail.com>
2024-07-14 21:07:08 +0300

    Update

--- _printf.c
+++ _printf.c
@@ -19,7 +19,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (!format || *format == '%')
+	if (!format)
 	{
 		return (-1);
 	}
@@ -51,10 +51,15 @@ int _printf(const char *format, ...)
 				}
 			}
 		}
+		else if (format[i] == '%' && format[i + 1] == '\0')
+		{
+			return (0);
+		}
 		else
 		{
 			count += _putchar(format[i]);
 		}
+
 		i++;
 	}
 	va_end(args);
