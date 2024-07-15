faf75be661f7ca3e22d4c04c5682ac06e0817dd9
isaiah-max <mlewaisaiah@gmail.com>
2024-07-15 11:44:44 +0000

    Update

--- _printf_int.c
+++ _printf_int.c
@@ -6,23 +6,26 @@
  *
  * Return: number of characters printed
  */
-int _printf_int(va_list args) {
+int _printf_int(va_list args)
+{
   int n = va_arg(args, int);
   char buffer[20];
   int i = 0, j, count = 0;
 
-  if (n < 0) {
-    _putchar('-');
-    n = -n;
-    count++;
-  }
+  if (n < 0)
+    {
+      count += _putchar('-');
+      n = -n;
+    }
   do {
-    buffer[i++] = (n % 10) + '0';
-    n /= 10;
-  } while (n > 0);
-  for (j = i - 1; j >= 0; j--) {
-    _putchar(buffer[j]);
-    count++;
-  }
+      buffer[i++] = (n % 10) + '0';
+      n /= 10;
+    }
+  while (n > 0);
+
+  for (j = i - 1; j >= 0; j--)
+    {
+      count += _putchar(buffer[j]);
+    }
   return (count);
 }
